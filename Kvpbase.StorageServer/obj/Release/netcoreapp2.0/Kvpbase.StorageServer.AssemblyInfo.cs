2ffd99b450d6329e93f7268adbd21a40584e4269
Joel Christner <joel@maraudersoftware.com>
2020-04-25 19:28:09 -0700

    Update to .NET Core 3.1 for Docker/cross-platform

--- Kvpbase.StorageServer/obj/Release/netcoreapp2.0/Kvpbase.StorageServer.AssemblyInfo.cs
+++ Kvpbase.StorageServer/obj/Release/netcoreapp2.0/Kvpbase.StorageServer.AssemblyInfo.cs
@@ -13,8 +13,8 @@ using System.Reflection;
 
 [assembly: System.Reflection.AssemblyCompanyAttribute("Joel Christner")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Release")]
-[assembly: System.Reflection.AssemblyCopyrightAttribute("(c)2019 Joel Christner")]
-[assembly: System.Reflection.AssemblyDescriptionAttribute("Object storage system with integrated replication and a simple RESTful API.")]
+[assembly: System.Reflection.AssemblyCopyrightAttribute("(c)2020 Joel Christner")]
+[assembly: System.Reflection.AssemblyDescriptionAttribute("Scalable, simple RESTful object storage platform.")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("4.1.1.0")]
 [assembly: System.Reflection.AssemblyInformationalVersionAttribute("4.1.1")]
 [assembly: System.Reflection.AssemblyProductAttribute("Kvpbase Storage Server")]
